91ff79f268beff98fe34c44db95b94734591eea0
Ryota <rytswd@gmail.com>
2020-08-06 23:34:54 +0100

    Comment out unused AWS setup

--- main/kubernetes/providers.tf
+++ main/kubernetes/providers.tf
@@ -2,10 +2,11 @@ provider "aws" {
   version = "~> 2.0"
   region  = var.aws_region
 
-  assume_role {
-    role_arn = local.assumed_role
-  }
+  # assume_role {
+  #   role_arn = local.assumed_role
+  # }
 }
+
 provider "google" {
   version = "~> 3.33.0"
   region  = var.gcp_region
