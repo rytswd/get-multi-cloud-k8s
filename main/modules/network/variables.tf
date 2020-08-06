9f9b798f60aac38c060bc17aa46c858ddb5349b0
Ryota <rytswd@gmail.com>
2020-08-06 20:19:03 +0100

    Add GCP networking setup (#6)
    * Add GCP networking setup
    
    * Correct resource name

---
+++ main/modules/network/variables.tf
@@ -0,0 +1,29 @@
+/* ----------------------------------------------------------------------------
+    AWS
+
+      Quick Reference:
+      - ap-east-1:      Hong Kong
+      - ap-northeast-1: Tokyo
+      - eu-west-1:      Ireland
+      - eu-west-2:      London
+      - us-east-1:      N. Virginia
+      - us-west-1:      N. California
+      - us west-2:      Oregon
+------------------------------------------------------------------------------- */
+variable "aws_region" {}
+
+variable "aws_cidr" {}
+
+/* ----------------------------------------------------------------------------
+    GCP
+
+      Quick Reference:
+      - asia-east1:      Taiwan
+      - asia-northeast1: Tokyo
+      - europe-west2:    London
+      - us-east4:        Northern Virginia
+      - us-west1:        Oregon
+------------------------------------------------------------------------------- */
+variable "gcp_region" {}
+
+variable "gcp_cidr" {}
