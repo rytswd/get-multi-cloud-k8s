bc72570f9728ebef46e3a74d3c70919b95a81f00
Ryota <rytswd@gmail.com>
2020-08-06 23:29:30 +0100

    Update filename

---
+++ main/kubernetes/main.tf
@@ -0,0 +1,12 @@
+module "networking" {
+  source = "./modules/network"
+  providers = {
+    google = google.v01
+  }
+
+  gcp_region = "europe-west2"
+  gcp_cidr   = "10.12.0.0/16"
+
+  aws_region = "eu-west-2"
+  aws_cidr   = "10.22.0.0/16"
+}
