cfce7f31affb57f5ac3d67faa5a084f0c4481d50
Ryota <rytswd@gmail.com>
2020-08-06 20:28:52 +0100

    Adjust GCP setup

--- main/modules/network/aws.tf
+++ main/modules/network/aws.tf
@@ -5,14 +5,14 @@ provider "aws" {
 
 data "aws_availability_zones" "available" {}
 
-resource "aws_vpc" "main" {
+resource "aws_vpc" "multi_cloud_aws" {
   cidr_block = var.aws_cidr
 
   enable_dns_hostnames = true
   enable_dns_support   = true
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "multi_cloud_aws" {
   count = 3
 
   cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 3, count.index)
--- main/modules/network/gcp.tf
+++ main/modules/network/gcp.tf
@@ -1,15 +1,15 @@
 provider "google" {
-  version = "~> 2.10"
+  version = "~> 3.33.0"
   region  = var.gcp_region
 }
 
-resource "google_compute_network" "main" {
-  name                    = "main"
+resource "google_compute_network" "multi_cloud_gcp" {
+  name                    = "multi-cloud-gcp"
   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "subnet" {
-  name          = "subnet"
+resource "google_compute_subnetwork" "multi_cloud_gcp_subnet" {
+  name          = "multi_cloud_gcp_subnet"
   ip_cidr_range = var.gcp_cidr
   network       = google_compute_network.main.self_link
 }
