6fcd45e9ca74a52dd2143fa3395f3e0b6451ff4b
Ryota <rytswd@gmail.com>
2020-08-08 00:34:38 +0100

    Remove module and simplify

--- init/gcp.tf
+++ init/gcp.tf
@@ -11,7 +11,7 @@ provider "google" {
   region  = var.gcp_region
   project = "get-multi-cloud-k8s-admin"
 }
-resource "google_project_service" "service" {
+resource "google_project_service" "admin" {
   for_each = toset([
     "compute.googleapis.com",
     "cloudresourcemanager.googleapis.com",
@@ -28,48 +28,30 @@ resource "google_project_service" "service" {
     GCP
       For rytswd-get-multi-cloud-k8s-v01
 ------------------------------------------------------------------------------- */
-locals {
-  project_v01 = "rytswd-get-multi-cloud-k8s-v01"
-}
-provider "google" {
-  version = "~> 3.33.0"
-  region  = var.gcp_region
-  # If used in Org setup with G Suite, this should be the parent Org. Because
-  # this setup assumes personal account, each project will be created outside
-  # of Terraform, and we use separate providers for each project.
-  project = local.project_v01
-  alias   = "v01"
-}
-module "gcp_init" {
-  source = "./gcp"
-  providers = {
-    google = google.v01
-  }
+resource "google_project_service" "v01" {
+  for_each = toset([
+    "compute.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+  ])
 
-  gcp_project = local.project_v01
+  service = each.key
+
+  project            = "rytswd-get-multi-cloud-k8s-v01"
+  disable_on_destroy = false
 }
 
-# /* ----------------------------------------------------------------------------
-#     GCP
-#       For rytswd-get-multi-cloud-k8s-v02
-# ------------------------------------------------------------------------------- */
-# locals {
-#   project_v02 = "rytswd-get-multi-cloud-k8s-v02"
-# }
-# provider "google" {
-#   version = "~> 3.33.0"
-#   region  = var.gcp_region
-#   # If used in Org setup with G Suite, this should be the parent Org. Because
-#   # this setup assumes personal account, each project will be created outside
-#   # of Terraform, and we use separate providers for each project.
-#   project = local.project_v02
-#   alias   = "v02"
-# }
-# module "gcp_v02" {
-#   source = "./gcp"
-#   providers = {
-#     google = google.v02
-#   }
+/* ----------------------------------------------------------------------------
+    GCP
+      For rytswd-get-multi-cloud-k8s-v02
+------------------------------------------------------------------------------- */
+resource "google_project_service" "v02" {
+  for_each = toset([
+    "compute.googleapis.com",
+    "cloudresourcemanager.googleapis.com",
+  ])
 
-#   gcp_project = local.project_v02
-# }
+  service = each.key
+
+  project            = "rytswd-get-multi-cloud-k8s-v02"
+  disable_on_destroy = false
+}
