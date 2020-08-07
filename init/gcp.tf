08cad02b36d8866352f49d7e7154b709ec14e87c
Ryota <rytswd@gmail.com>
2020-08-08 00:22:56 +0100

    Temporarily keep original module

--- init/gcp.tf
+++ init/gcp.tf
@@ -24,30 +24,30 @@ resource "google_project_service" "service" {
 }
 
 
-# /* ----------------------------------------------------------------------------
-#     GCP
-#       For rytswd-get-multi-cloud-k8s-v01
-# ------------------------------------------------------------------------------- */
-# locals {
-#   project_v01 = "rytswd-get-multi-cloud-k8s-v01"
-# }
-# provider "google" {
-#   version = "~> 3.33.0"
-#   region  = var.gcp_region
-#   # If used in Org setup with G Suite, this should be the parent Org. Because
-#   # this setup assumes personal account, each project will be created outside
-#   # of Terraform, and we use separate providers for each project.
-#   project = local.project_v01
-#   alias   = "v01"
-# }
-# module "gcp_init" {
-#   source = "./gcp"
-#   providers = {
-#     google = google.v01
-#   }
+/* ----------------------------------------------------------------------------
+    GCP
+      For rytswd-get-multi-cloud-k8s-v01
+------------------------------------------------------------------------------- */
+locals {
+  project_v01 = "rytswd-get-multi-cloud-k8s-v01"
+}
+provider "google" {
+  version = "~> 3.33.0"
+  region  = var.gcp_region
+  # If used in Org setup with G Suite, this should be the parent Org. Because
+  # this setup assumes personal account, each project will be created outside
+  # of Terraform, and we use separate providers for each project.
+  project = local.project_v01
+  alias   = "v01"
+}
+module "gcp_init" {
+  source = "./gcp"
+  providers = {
+    google = google.v01
+  }
 
-#   gcp_project = local.project_v01
-# }
+  gcp_project = local.project_v01
+}
 
 # /* ----------------------------------------------------------------------------
 #     GCP
