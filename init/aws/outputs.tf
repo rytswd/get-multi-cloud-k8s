37b773e2b4c2852c1f31e31258f25610770df6d0
Ryota <rytswd@gmail.com>
2020-08-10 20:38:49 +0100

    Correct variable reference

--- init/aws/outputs.tf
+++ init/aws/outputs.tf
@@ -5,6 +5,9 @@ output "terraform_admin_access_key_encrytped_secret" {
   value = aws_iam_access_key.terraform_admin.encrypted_secret
 }
 
-output "account_id" {
-  value = aws_organizations_account.account.id
+output "v01_id" {
+  value = aws_organizations_account.v01.id
+}
+output "v02_id" {
+  value = aws_organizations_account.v02.id
 }
