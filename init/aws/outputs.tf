0f32bfcff09ee8cd27e24e71bb51329b7f8742e2
Ryota <rytswd@gmail.com>
2020-08-10 20:58:41 +0100

    Put back key

--- init/aws/outputs.tf
+++ init/aws/outputs.tf
@@ -1,9 +1,9 @@
-# output "terraform_admin_access_key_id" {
-#   value = aws_iam_access_key.terraform_admin.id
-# }
-# output "terraform_admin_access_key_encrytped_secret" {
-#   value = aws_iam_access_key.terraform_admin.encrypted_secret
-# }
+output "terraform_admin_access_key_id" {
+  value = aws_iam_access_key.terraform_admin.id
+}
+output "terraform_admin_access_key_encrytped_secret" {
+  value = aws_iam_access_key.terraform_admin.encrypted_secret
+}
 
 output "v01_id" {
   value = aws_organizations_account.v01.id
--- init/outputs.tf
+++ init/outputs.tf
@@ -7,12 +7,12 @@ output "aws_v01_account_id" {
 output "aws_v02_account_id" {
   value = module.aws_init.v02_id
 }
-# output "aws_terraform_admin_access_key_id" {
-#   value = module.aws_init.terraform_admin_access_key_id
-# }
-# output "aws_terraform_admin_access_key_encrypted_secret" {
-#   value = module.aws_init.terraform_admin_access_key_encrytped_secret
-# }
+output "aws_terraform_admin_access_key_id" {
+  value = module.aws_init.terraform_admin_access_key_id
+}
+output "aws_terraform_admin_access_key_encrypted_secret" {
+  value = module.aws_init.terraform_admin_access_key_encrytped_secret
+}
 
 /* ----------------------------------------------------------------------------
     GCP
