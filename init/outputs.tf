5ccce913902220eacfa7b45094072d3d27e9e441
Ryota <rytswd@gmail.com>
2020-08-10 20:41:47 +0100

    Correct output source

--- init/outputs.tf
+++ init/outputs.tf
@@ -1,8 +1,11 @@
 /* ----------------------------------------------------------------------------
     AWS
 ------------------------------------------------------------------------------- */
-output "aws_account_id" {
-  value = module.aws_init.account_id
+output "aws_v01_account_id" {
+  value = module.aws_init.v01_id
+}
+output "aws_v02_account_id" {
+  value = module.aws_init.v02_id
 }
 output "aws_terraform_admin_access_key_id" {
   value = module.aws_init.terraform_admin_access_key_id
