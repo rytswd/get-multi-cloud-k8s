36da22d0f8ea699edee603e12db57464567077db
Ryota <rytswd@gmail.com>
2020-08-10 10:01:25 +0100

    Add variable comments

--- main/kubernetes/variables.tf
+++ main/kubernetes/variables.tf
@@ -1,3 +1,15 @@
+/* ----------------------------------------------------------------------------
+    AWS
+
+      Quick Reference:
+      - ap-east-1:      Hong Kong
+      - ap-northeast-1: Tokyo
+      - eu-west-1:      Ireland
+      - eu-west-2:      London
+      - us-east-1:      N. Virginia
+      - us-west-1:      N. California
+      - us west-2:      Oregon
+------------------------------------------------------------------------------- */
 variable "aws_region" {
   default = "eu-west-2"
 }
@@ -7,6 +19,16 @@ variable "aws_account" {
   default     = "953930786689"
 }
 
+/* ----------------------------------------------------------------------------
+    GCP
+
+      Quick Reference:
+      - asia-east1:      Taiwan
+      - asia-northeast1: Tokyo
+      - europe-west2:    London
+      - us-east4:        Northern Virginia
+      - us-west1:        Oregon
+------------------------------------------------------------------------------- */
 variable "gcp_region" {
   default = "europe-west2"
 }
