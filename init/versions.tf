5d5bbabf261a9041d0191a911580651f20f0244b
Ryota <rytswd@gmail.com>
2020-08-10 20:56:58 +0100

    Add version restrictions to providers

--- init/versions.tf
+++ init/versions.tf
@@ -1,10 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 3.1.0"
     }
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
+      version = "~> 3.33.0"
     }
   }
   required_version = ">= 0.13"
