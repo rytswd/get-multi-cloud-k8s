b68d63b175c935b3179f3b552681e0d1271af320
Ryota <rytswd@gmail.com>
2020-08-11 18:03:10 +0100

    Remove version constraint from provider and rely on versions file

--- main/kubernetes/providers.tf
+++ main/kubernetes/providers.tf
@@ -1,7 +1,6 @@
 provider "aws" {
-  version = "~> 2.0"
-  region  = var.aws_region
-  alias   = "v01"
+  region = var.aws_region
+  alias  = "v01"
 
   assume_role {
     role_arn = local.aws_account_role
@@ -9,14 +8,12 @@ provider "aws" {
 }
 
 provider "google" {
-  version = "~> 3.33.0"
   region  = var.gcp_region
   project = var.gcp_project
   alias   = "v01"
 }
 
 provider "google-beta" {
-  version = "~> 3.33.0"
   region  = var.gcp_region
   project = var.gcp_project
   alias   = "v01"
