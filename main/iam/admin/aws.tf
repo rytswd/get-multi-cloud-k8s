1650fd3641c0f5a4b91a70be42fda91f78fd715f
Ryota <rytswd@gmail.com>
2020-08-20 21:16:58 +0100

    Add module

---
+++ main/iam/admin/aws.tf
@@ -0,0 +1,27 @@
+module "aws_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "2.18.0"
+
+  name    = var.aws_username
+  pgp_key = "keybase:${var.aws_keybase_userid}"
+
+  force_destroy           = true
+  password_reset_required = true
+}
+data "aws_iam_policy_document" "user" {
+  statement {
+    effect    = "Allow"
+    actions   = ["sts:AssumeRole"]
+    resources = var.aws_resources
+  }
+}
+resource "aws_iam_user_policy" "user" {
+  name   = "AssumeRoleFor_${var.aws_username}"
+  user   = module.aws_user.this_iam_user_name
+  policy = data.aws_iam_policy_document.user.json
+}
+
+// Can be commented out once used
+output "user_keybase_decrypt" {
+  value = module.aws_user.keybase_password_decrypt_command
+}
---
+++ main/iam/admin/variables.tf
@@ -0,0 +1,5 @@
+variable "aws_username" {}
+variable "aws_resources" {
+  type = list(any)
+}
+variable "aws_keybase_userid" {}
