0baa41e513bd1494cc08ce42fbe42d4d2096aa15
Ryota <rytswd@gmail.com>
2020-08-20 23:20:22 +0100

    Correct variable name

--- main/iam/users_admin.tf
+++ main/iam/users_admin.tf
@@ -5,7 +5,7 @@ module "rytswd" {
   aws_keybase_userid = "rytswd"
   aws_resources = [
     local.aws_iam_admin,
-    local.aws_v91_admin,
+    local.aws_v01_admin,
     local.aws_v02_admin,
   ]
 }
--- main/iam/variables.tf
+++ main/iam/variables.tf
@@ -14,7 +14,7 @@ locals {
   aws_admin_role_name = data.terraform_remote_state.init.outputs.aws_admin_role_name
 
   aws_iam_admin = "arn:aws:iam::${data.terraform_remote_state.init.outputs.aws_account_iam_id}:role/${local.aws_admin_role_name}"
-  aws_v91_admin = "arn:aws:iam::${data.terraform_remote_state.init.outputs.aws_v01_account_id}:role/${local.aws_admin_role_name}"
+  aws_v01_admin = "arn:aws:iam::${data.terraform_remote_state.init.outputs.aws_v01_account_id}:role/${local.aws_admin_role_name}"
   aws_v02_admin = "arn:aws:iam::${data.terraform_remote_state.init.outputs.aws_v02_account_id}:role/${local.aws_admin_role_name}"
 }
 
