28f62447f21e5f15a8d646a265d5587525cc8028
Ryota <rytswd@gmail.com>
2020-12-29 23:01:52 +0000

    Add PGP output

--- main/iam/modules/user/aws.tf
+++ main/iam/modules/user/aws.tf
@@ -25,3 +25,7 @@ resource "aws_iam_user_policy" "user" {
 output "user_keybase_decrypt" {
   value = module.aws_user.keybase_password_decrypt_command
 }
+
+output "user_keybase_pgp" {
+  value = module.aws_user.keybase_password_pgp_message
+}
