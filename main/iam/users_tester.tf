33ce5ca70e738478c0e123203a13bc2f82b26e9d
Ryota <rytswd@gmail.com>
2020-12-30 01:21:01 +0000

    Disable a tester

--- main/iam/users_tester.tf
+++ main/iam/users_tester.tf
@@ -1,14 +1,14 @@
-module "some_tester" {
-  source = "./modules/user"
+# module "some_tester" {
+#   source = "./modules/user"
 
-  aws_username       = "new_tester"
-  aws_keybase_userid = "rytswd"
-  aws_resources = [
-    local.aws_iam_member,
-    local.aws_v01_member,
-  ]
-}
-// Can be commented out once used
-output "some_tester_keybase_decrypt" {
-  value = module.some_tester.user_keybase_pgp
-}
+#   aws_username       = "new_tester"
+#   aws_keybase_userid = "rytswd"
+#   aws_resources = [
+#     local.aws_iam_member,
+#     local.aws_v01_member,
+#   ]
+# }
+# // Can be commented out once used
+# output "some_tester_keybase_decrypt" {
+#   value = module.some_tester.user_keybase_pgp
+# }
