769aab83c001f0bc044856707b94baedf1cb2ac7
Ryota <rytswd@gmail.com>
2020-12-30 00:19:38 +0000

    Delete admin user temporarily

--- main/iam/users_admin.tf
+++ main/iam/users_admin.tf
@@ -1,15 +1,15 @@
-module "rytswd" {
-  source = "./modules/user"
+# module "rytswd" {
+#   source = "./modules/user"
 
-  aws_username       = "rytswd"
-  aws_keybase_userid = "rytswd"
-  aws_resources = [
-    local.aws_iam_admin,
-    local.aws_v01_admin,
-    local.aws_v02_admin,
-  ]
-}
-// Can be commented out once used
-output "rytswd_keybase_decrypt" {
-  value = module.rytswd.user_keybase_decrypt
-}
+#   aws_username       = "rytswd"
+#   aws_keybase_userid = "rytswd"
+#   aws_resources = [
+#     local.aws_iam_admin,
+#     local.aws_v01_admin,
+#     local.aws_v02_admin,
+#   ]
+# }
+# // Can be commented out once used
+# output "rytswd_keybase_decrypt" {
+#   value = module.rytswd.user_keybase_decrypt
+# }
