18063d6fd2b3916f43432877a90073c137776a73
Ryota <rytswd@gmail.com>
2020-12-30 00:07:48 +0000

    Add default

--- main/iam/modules/user/variables.tf
+++ main/iam/modules/user/variables.tf
@@ -2,5 +2,9 @@ variable "aws_username" {}
 variable "aws_resources" {
   type = list(any)
 }
-variable "aws_keybase_userid" {}
-variable "aws_pgp_key" {}
+variable "aws_keybase_userid" {
+  default = ""
+}
+variable "aws_pgp_key" {
+  default = ""
+}
